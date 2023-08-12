32b4ba8283ecdde2a396dde3f11d575ae98badd0
emadhegazy <acc.ekh1@gmail.com>
2023-08-12 14:21:37 -0700

    to check the character if it is a delimiter

--- utilty_functions.c
+++ utilty_functions.c
@@ -11,3 +11,17 @@ int interactive(info_t *info)
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
+/**
+ * is_delim - is to check the character if it is a delimiter.
+ * @c: this is the  char to be checked in the function.
+ * @delim: is the given delimiter string.
+ * Return: if true return 1,  if false return 0.
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
