c11a94e9d5ea963b6b9f54d14823969c72b6203c
GarismoHussien <hand2land@gmail.com>
2023-08-13 06:42:09 -0700

    add shell

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
