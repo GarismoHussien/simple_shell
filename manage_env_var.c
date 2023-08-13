2dd3b8e4d769c63eadaf2d1a593fe307a10c580b
emadhegazy <acc.ekh1@gmail.com>
2023-08-13 14:31:14 -0700

    delete or clear an environment variable

--- manage_env_var.c
+++ manage_env_var.c
@@ -50,4 +50,24 @@ int _mysetenv(info_t *info)
 		return (0);
 	return (1);
 }
+/**
+ * _myunsetenv - this is to delete or clear an environment variable.
+ *
+ * @info: this is a structure type of type info_t.
+ * Return: 0 is always returned.
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
 
