a0143b6fd84fad0a377a4d90afbaaf593f2e1b12
GarismoHussien <hand2land@gmail.com>
2023-08-13 13:40:39 -0700

    add string file

--- string_shell_utils.c
+++ string_shell_utils.c
@@ -40,4 +40,32 @@ return (s);
  while (*s++ != '\0');
 return (NULL);
 }
+/**
+ **_strncpy - copies a string
+ *@dest: destination string to be copied to
+ *@src: source string
+ *@n: amount of characters to be copied
+ *Return: concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, j;
+char *s = dest;
+i = 0;
+while (src[i] != '\0' && i < n - 1)
+{
+dest[i] = src[i];
+i++;
+}
+if (i < n)
+{
+j = i;
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+}
+return (s);
+}
 
