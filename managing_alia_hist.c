12811ce5ca140d52670d05fe8aa47a61d34a8f78
emadhegazy <acc.ekh1@gmail.com>
2023-08-13 06:00:20 -0700

    copy the alias builtin command

--- managing_alia_hist.c
+++ managing_alia_hist.c
@@ -54,4 +54,36 @@ int set_alias(info_t *info, char *str)
 	unset_alias(info, str);
 	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
+/**
+ * _myalias - the purpose is to copy the alias builtin command.
+ * @info: is a structure type containing potential arguments,
+ *         Used to maintain constant function prototype.
+ *  Return: if success 0 is returned or 1 on error.
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
 
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
