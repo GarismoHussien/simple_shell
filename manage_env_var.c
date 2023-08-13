8ba8558db4f792dfae7561096abf99464ce3dfee
emadhegazy <acc.ekh1@gmail.com>
2023-08-13 14:19:01 -0700

    to take the value of a specific environment variable

--- manage_env_var.c
+++ manage_env_var.c
@@ -11,4 +11,25 @@ int _myenv(info_t *info)
 	print_list_str(info->env);
 	return (0);
 }
+/**
+ * _getenv - this is to take the value of a specific environment variable.
+ * @info: is a character pointer and a type of structure.
+ * @name: is the name of environment variable
+ *
+ * Return: the value returns NULL
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
 
