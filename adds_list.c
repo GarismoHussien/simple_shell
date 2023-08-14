3936bda3a8749ceb427cf870db44c4dc7aa6db69
GarismoHussien <hand2land@gmail.com>
2023-08-14 11:25:58 -0700

    lists

--- adds_list.c
+++ adds_list.c
@@ -7,19 +7,54 @@
  */
 void free_list(list_t **head_ptr)
 {
-	list_t *node, *next_node, *head;
-
-	if (!head_ptr || !*head_ptr)
-		return;
-	head = *head_ptr;
-	node = head;
-	while (node)
-	{
-		next_node = node->next;
-		free(node->str);
-		free(node);
-		node = next_node;
-	}
-	*head_ptr = NULL;
+list_t *node, *next_node, *head;
+if (!head_ptr || !*head_ptr)
+return;
+head = *head_ptr;
+node = head;
+while (node)
+{
+next_node = node->next;
+free(node->str);
+free(node);
+node = next_node;
 }
-
+*head_ptr = NULL;
+}
+/**
+ * delete_node_at_index - deletes node at given index
+ * @head: address of pointer to first node
+ * @index: index of node to delete
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+list_t *node, *prev_node;
+unsigned int i = 0;
+if (!head || !*head)
+return (0);
+if (!index)
+{
+node = *head;
+*head = (*head)->next;
+free(node->str);
+free(node);
+return (1);
+}
+node = *head;
+while (node)
+{
+if (i == index)
+{
+prev_node->next = node->next;
+free(node->str);
+free(node);
+return (1);
+}
+i++;
+prev_node = node;
+node = node->next;
+}
+return (0);
+}
