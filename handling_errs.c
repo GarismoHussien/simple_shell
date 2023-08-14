aedebbd57d1b40c5253611d0e02517a0dd0a369f
emadhegazy <acc.ekh1@gmail.com>
2023-08-14 04:48:56 -0700

    to write the character c to a given fd file

--- handling_errs.c
+++ handling_errs.c
@@ -40,4 +40,26 @@ int _eputchar(char c)
 		buf[i++] = c;
 	return (1);
 }
+/**
+ * _putfd - this function is to write the character c to a given fd file.
+ * @c: takes the character to print.
+ * @fd: this is the file descriptor to write in.
+ *
+ * Return: if success return 1.
+ * On error,  return -1, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static char buf[WRITE_BUF_SIZE];
+	static int i;
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
 
