0dd7a6ad1236356a7a70da38c951f31817d92bab
emadhegazy <acc.ekh1@gmail.com>
2023-08-14 04:43:03 -0700

    to write the character c to stderr

--- handling_errs.c
+++ handling_errs.c
@@ -19,3 +19,25 @@ void _eputs(char *str)
 		i++;
 	}
 }
+/**
+ * _eputchar - this function is to write the character c to stderr.
+ * @c: takes the character to print.
+ *
+ * Return: if it is succeeded return 1.
+ * On error,  return -1, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static char buf[WRITE_BUF_SIZE];
+	static int i;
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
