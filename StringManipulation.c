ed2cf4911e3900a24a92224aec4430dec203d4b4
GarismoHussien <hand2land@gmail.com>
2023-08-14 04:14:13 -0700

    edit String

--- StringManipulation.c
+++ StringManipulation.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained commands,reads input from the user(buff chanined cmd)
- * @info: parameter struct containing information about the shell(param. struct.)
- * @buf: address of buffer where  input will be stored(address).
+ * input_buf - buffers chained commands,reads input from user(buff chanined cmd
+ * @info: parameter struct containing information about the shell(param
+ * @buf: address of buffer where  input will be stored(address
  * @len: address of len var representing the length of the buffer
  *
  * Return: bytes read
@@ -100,4 +100,43 @@ if (r >= 0)
 *i = r;
 return (r);
 }
-
+/**
+ * _getline - gets next line of input from STDIN
+ * @info: param.r struct
+ * @ptr: address of pointer to buff, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
+ *
+ * Return: s
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+static char buf[READ_BUF_SIZE];
+static size_t i, len;
+size_t k;
+ssize_t r = 0, s = 0;
+char *p = NULL, *new_p = NULL, *c;
+p = *ptr;
+if (p && length)
+s = *length;
+if (i == len)
+i = len = 0;
+r = read_buf(info, buf, &len);
+if (r == -1 || (r == 0 && len == 0))
+return (-1);
+c = _strchr(buf + i, '\n');
+k = c ? 1 + (unsigned int)(c - buf) : len;
+new_p = _realloc(p, s, s ? s + k : k + 1);
+if (!new_p) /* MALLOC FAILURE */
+return (p ? free(p), -1 : -1);
+if (s)
+_strncat(new_p, buf + i, k - i);
+else
+_strncpy(new_p, buf + i, k - i + 1);
+s += k - i;
+i = k;
+p = new_p;
+if (length)
+*length = s;
+*ptr = p;
+return (s);
+}
