0799551c08a6aa77474a9b0fb4fd4595de81d327
emadhegazy <acc.ekh1@gmail.com>
2023-08-14 13:55:09 -0700

    to read the history data

--- history_linked_list.c
+++ history_linked_list.c
@@ -53,4 +53,52 @@ int write_history(info_t *info)
 	close(fd);
 	return (1);
 }
+/**
+ * read_history - this function is  to read the history data,
+ *            from the history file.
+ * @info: is a pointer of a struct type and the parameter.
+ *
+ * Return: history count on success, or 0 if fails.
+ */
+int read_history(info_t *info)
+{
+	struct stat st;
+	char *buf = NULL, *filename = get_history_file(info);
+	int i, last = 0, linecount = 0;
+	ssize_t fd, rdlen, fsize = 0;
 
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	if (fd == -1)
+		return (0);
+	if (!fstat(fd, &st))
+		fsize = st.st_size;
+	if (fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fsize + 1));
+	if (!buf)
+		return (0);
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
+	if (rdlen <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_history_list(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	if (last != i)
+		build_history_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
+}
