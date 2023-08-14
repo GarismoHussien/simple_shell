0f5af4e3684dec1d8e2fac86d4b4b4697f03dd79
GarismoHussien <hand2land@gmail.com>
2023-08-14 15:21:04 -0700

    fix shell_file to execute file

--- execute_external_cmd.c
+++ execute_external_cmd.c
@@ -79,4 +79,42 @@ print_error(info, "Permission denied\n");
 }
 }
 }
-
+/**
+ * find_cmd - Locate and execute a command in the PATH environment.
+ * @info: Pointer to the shell information struct containing command details.
+ *This function searches for a specified command in the directories
+ * Return: void
+ */
+void find_cmd(info_t *info)
+{
+char *path = NULL;
+int i, k;
+info->path = info->argv[0];
+if (info->linecount_flag == 1)
+{
+info->line_count++;
+info->linecount_flag = 0;
+}
+for (i = 0, k = 0; info->arg[i]; i++)
+if (!is_delim(info->arg[i], " \t\n"))
+k++;
+if (!k)
+return;
+path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
+if (path)
+{
+info->path = path;
+fork_cmd(info);
+}
+else
+{
+if ((interactive(info) || _getenv(info, "PATH=")
+|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+fork_cmd(info);
+else if (*(info->arg) != '\n')
+{
+info->status = 127;
+print_error(info, "not found\n");
+}
+}
+}
