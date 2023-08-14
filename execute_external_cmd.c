8f6e5109708ae4c6f2edcca7dbea4354b04ca73a
GarismoHussien <hand2land@gmail.com>
2023-08-14 15:18:16 -0700

    fix shell_file to execute file

--- execute_external_cmd.c
+++ execute_external_cmd.c
@@ -41,3 +41,42 @@ exit(info->err_num);
 }
 return (builtin_ret);
 }
+/**
+ * fork_cmd - Forks a new process and executes a cmd
+ * @info:  Pointer to the shell information struct containing command details.
+ *This function creates a child process using the fork() system call. The child
+ * Return: void
+ */
+void fork_cmd(info_t *info)
+{
+pid_t child_pid;
+child_pid = fork();
+if (child_pid == -1)
+{
+/* TODO: PUT ERROR FUNCON */
+perror("Error:");
+return;
+}
+if (child_pid == 0)
+{
+if (execve(info->path, info->argv, get_environ(info)) == -1)
+{
+free_info(info, 1);
+if (errno == EACCES)
+exit(126);
+exit(1);
+}
+/* TODO: PUT ERROR FUNCON */
+}
+else
+{
+wait(&(info->status));
+if (WIFEXITED(info->status))
+{
+info->status = WEXITSTATUS(info->status);
+if (info->status == 126)
+print_error(info, "Permission denied\n");
+}
+}
+}
+
