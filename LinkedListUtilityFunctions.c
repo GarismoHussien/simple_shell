2ace0ba3d3b22f69997116d7c0efe2b4e9999bf6
GarismoHussien <hand2land@gmail.com>
2023-08-15 18:31:14 -0700

    linkedlist.c edit

--- LinkedListUtilityFunctions.c
+++ LinkedListUtilityFunctions.c
@@ -56,3 +56,24 @@ node = node->next;
 }
 return (NULL);
 }
+/**
+ * print_list - prints all elements 
+ * @h: pointer to first nodee
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+_puts(convert_number(h->num, 10, 0));
+_putchar(':');
+_putchar(' ');
+_puts(h->str ? h->str : "(nil)");
+_puts("\n");
+h = h->next;
+i++;
+}
+return (i);
+}
