5c46df7cf2dcf7c5a8606d4f7469edad6ae913d0
emadhegazy <acc.ekh1@gmail.com>
2023-08-19 07:11:51 -0700

    correct an error in printing the alial

--- handles.c
+++ handles.c
@@ -28,22 +28,40 @@ int _myexit(info_t *info)
 	info->err_num = -1;
 		return (-2);
 }
+
 /**
- * _mycd - this function changes the current directory of the process.
- * @info: a type of structure that contains the potential arguments,
- *      and Used to maintain constant function prototype.
+ * _myhelp - this is to change the current directory of the process.
+ * @info: this is a structure type that has various information,
+ *  Used to maintain constant function prototype.
+ * Return: 0 is always returned.
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
+/**
+ * _mycd - this function changes the current directory of the process
+ * @info:  a type of structure that contains the potential arguments
+ * and  Used to maintain constant function prototype.
  * Return: 0 is always returned.
  */
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
-s = getcwd(buffer, 1024);
+
+	s = getcwd(buffer, 1024);
 	if (!s)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
-dir = _getenv(info, "HOME=");
+		dir = _getenv(info, "HOME=");
 		if (!dir)
 			chdir_ret = /* TODO: what should this be? */
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
@@ -51,6 +69,7 @@ dir = _getenv(info, "HOME=");
 			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
+	{
 		if (!_getenv(info, "OLDPWD="))
 		{
 			_puts(s);
@@ -73,22 +92,6 @@ dir = _getenv(info, "HOME=");
 		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
 		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
-	return (0);
-}
-/**
- * _myhelp - this is to change the current directory of the process.
- * @info: this is a structure type that has various information,
- *  Used to maintain constant function prototype.
- * Return: 0 is always returned.
- */
-int _myhelp(info_t *info)
-{
-	char **arg_array;
-
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
 
--- utilty_functions.c
+++ utilty_functions.c
@@ -12,32 +12,6 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - is to check the character if it is a delimiter.
- * @c: this is the  char to be checked in the function.
- * @delim: is the given delimiter string.
- * Return: if true return 1,  if false return 0.
- */
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
-/**
- * _isalpha - using the ascii code to check the alphabetic character.
- * @c: The inputted character.
- * Return: if c is alphabetic return 1 or 0 if false.
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-/**
  * _atoi - converts a string to an integer
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
@@ -70,4 +44,30 @@ int _atoi(char *s)
 
 	return (output);
 }
+/**
+ * is_delim - is to check the character if it is a delimiter.
+ * @c: this is the  char to be checked in the function.
+ * @delim: is the given delimiter string.
+ * Return: if true return 1,  if false return 0.
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
 
+/**
+ * _isalpha - using the ascii code to check the alphabetic character.
+ * @c: The inputted character.
+ * Return: if c is alphabetic return 1 or 0 if false.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
