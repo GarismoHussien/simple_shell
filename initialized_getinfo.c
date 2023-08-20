b765e03c7a28af730d3da62cee37f9776da90349
GarismoHussien <hand2land@gmail.com>
2023-08-20 05:23:02 -0700

    fix file

--- initialized_getinfo.c
+++ initialized_getinfo.c
@@ -45,23 +45,27 @@ _putchar(BUF_FLUSH);
  */
 void set_info(info_t *info, char **av)
 {
-int i = 0;
-info->fname = av[0];
-if (info->arg)
-{
-info->argv = strtow(info->arg, " \t");
-if (!info->argv)
-{
-info->argv = malloc(sizeof(char *) * 2);
-if (info->argv)
-{
-info->argv[0] = _strdup(info->arg);
-info->argv[1] = NULL;
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
 }
-}
-}
-for (i = 0; info->argv && info->argv[i]; i++);
-info->argc = i;
-replace_alias(info);
-replace_vars(info);
-}
+
